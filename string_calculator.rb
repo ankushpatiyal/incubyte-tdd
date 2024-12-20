d2ed5ba026d217708e507b1e29c17c08e85fcca1
ankush <ankushpatiyal@gmail.com>
2024-12-20 14:46:18 +0530

    Adding code to add numbers given in the numbers string the base case

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,6 +3,7 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
 
-    return numbers.to_i
+    extracted_numbers = numbers.split(",").map(&:to_i)
+    extracted_numbers.sum
   end
 end
