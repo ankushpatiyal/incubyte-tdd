4b7242482641cd3fe2fc515d9d31ed5185c62b82
ankush <ankushpatiyal@gmail.com>
2024-12-20 16:32:46 +0530

    Adding error class and its test cases

---
+++ errors.rb
@@ -0,0 +1,13 @@
+module Errors
+  class NegativeInputError < StandardError
+    NEGATIVE_INPUT_MESSAGE = 'Negative numbers not allowed'
+
+    def initialize(numbers)
+      super(custom_error_message)
+    end
+
+    def custom_error_message
+      "#{NEGATIVE_INPUT_MESSAGE} #{@numbers.join(',')}"
+    end
+  end
+end
